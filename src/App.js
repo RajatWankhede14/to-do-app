3f5d302d166f10cc7a33aae7ae7ff34f209d1d4d
Rajat Wankhede <rajatwankhede@Rajats-MacBook-Pro.local>
2021-01-31 19:00:58 +0530

    added some material-ul styles to input and button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
+import { FormControl, InputLabel, Input, Button  } from '@material-ui/core';
+import Todo from './Todo'
 
 function App() {
 
-  const [todos, setTodos] = useState(['code everyday', 'read books']);
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+
+  const addTodo = event => {
+    event.preventDefault(); // stop the refresh
+    setTodos([...todos, input]); // add new todo while keeping old todos
+    setInput(''); // clears the input field
+  }
   return (
     <div className="App">
       <h1>Hi Ender Let's Code 🚀</h1>
-
-      <input type="text" value={input} onChange={event => setInput(event.target.value)} />
-      <button>Add</button>
-
+      <form>
+      <FormControl>
+        <InputLabel>Write a Task</InputLabel>
+        <Input type="text" value={input} onChange={event => setInput(event.target.value)} />
+      </FormControl>
+      <Button type="submit" disabled={!input} variant="contained" color="primary" onClick={addTodo}>Add</Button>
+      </form>
       <ul>
         {todos.map(todo => (
-          <li>{todo}</li>
+          <Todo text={todo}/>
         ))}
       </ul>
     </div>
